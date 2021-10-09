30183cc323be057d8ddbde22b6cf0fe18a52e205
Raymond Ottun <sayjava@gmail.com>
2021-10-09 16:31:31 +0100

    feat: disconnected warning

--- ui/src/components/ServerStatus.tsx
+++ ui/src/components/ServerStatus.tsx
@@ -1,4 +1,5 @@
 import { ApiFilled } from '@ant-design/icons';
+import { notification, Button } from 'antd';
 import { useServerState } from './Provider';
 
 export const ServerStatus = () => {
@@ -9,6 +10,27 @@ export const ServerStatus = () => {
     if (connected) {
         return <></>;
     }
+
+    const doRefresh = () => {
+        notification.close(key);
+        window.location.reload();
+    };
+
+    const key = `disconnected`;
+    const reloadBtn = (
+        <Button type="primary" size="small" onClick={doRefresh}>
+            Reload Page
+        </Button>
+    );
+
+    notification.warning({
+        message: 'Server Disconnected',
+        description: 'The dashboard has disconnected from the server',
+        duration: null,
+        placement: 'topLeft',
+        key,
+        btn: reloadBtn,
+    });
 
     return <ApiFilled style={{ color: '#ff0000' }} />;
 };
