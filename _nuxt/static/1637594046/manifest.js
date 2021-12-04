d91e342e31dad75bbe32b87c816b5dd42eca9b33
sayjava <sayjava@users.noreply.github.com>
2021-12-04 14:06:40 +0000

    deploy: 603e1d793de6aa20b2219aaa0f76244cad2ef592

--- _nuxt/static/1637594046/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fapi","\u002Fassertions","\u002Fstart","\u002Fmocking","\u002Fdashboard"]})
---
+++ _nuxt/static/1638626798/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002Fstart","\u002Fmocking","\u002Fassertions","\u002Fapi","\u002Fdashboard"]})
