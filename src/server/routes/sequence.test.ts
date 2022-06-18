5e6115ba25dab48a104511a37a79b929910a0441
Raymond Ottun <sayjava@gmail.com>
2022-06-18 09:28:26 +0100

    fix: refactored sequence to use json

--- src/server/routes/sequence.test.ts
+++ src/server/routes/sequence.test.ts
@@ -6,79 +6,82 @@ jest.mock('fs', () => {
     return {
         existsSync: jest.fn(() => true),
         readFileSync: jest.fn(),
-        readdirSync: () => ['mocks.yml'],
+        readdirSync: () => ['mocks.json'],
         statSync: () => ({ isDirectory: () => true }),
     };
 });
 
-beforeEach(() => {
-    // @ts-ignore: Jest Mock
-    fs.readFileSync.mockReset();
-});
+describe('Request Sequence', () => {
+    beforeEach(() => {
+        // @ts-ignore: Jest Mock
+        fs.readFileSync.mockReset();
 
-test('return a 406  for requests less than 2', async () => {
-    // @ts-ignore: Jest Mock
-    fs.readFileSync.mockReturnValueOnce(`
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: GET
-            response:
-                body: Query worked
+        // @ts-ignore: Jest Mock
+        fs.readFileSync.mockReturnValueOnce(
+            JSON.stringify([
+                {
+                    name: 'test expectations',
+                    request: {
+                        path: '/tasks',
+                        method: 'GET',
+                    },
+                    response: {
+                        body: 'Query worked',
+                    },
+                },
+                {
+                    name: 'test expectations',
+                    request: {
+                        path: '/tasks',
+                        method: 'POST',
+                    },
+                    response: {
+                        body: 'Query worked',
+                    },
+                },
+            ]),
+        );
+    });
 
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: POST
-            response:
-                body: Query worked
-    `);
-    const { apiServer } = await createServer({});
-    const res = await request(apiServer)
-        .put('/api/requests/sequence')
-        .set('content-type', 'application/x-yaml')
-        .send(`-`);
-    expect(res.status).toBe(406);
-    expect(res.body).toMatchInlineSnapshot(`
-        Object {
-          "actual": "Received 1 requests",
-          "expected": "At least 2 requests",
-          "message": "At least 2 requests is needed for verifying a sequence",
-          "records": Array [],
-        }
-    `);
-});
+    it('return a 406  for requests less than 2', async () => {
+        const { apiServer } = await createServer({});
+        const res = await request(apiServer)
+            .put('/api/requests/sequence')
+            .set('content-type', 'application/json')
+            .send(JSON.stringify([]));
 
-test('return the error from a failed verification', async () => {
-    // @ts-ignore: Jest Mock
-    fs.readFileSync.mockReturnValueOnce(`
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: GET
-            response:
-                body: Query worked
-
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: POST
-            response:
-                body: Query worked
-    `);
-
-    const { apiServer } = await createServer({});
-
-    const res = await request(apiServer).put('/api/requests/sequence').set('content-type', 'application/x-yaml').send(`
-        - path: /tasks
-          method: POST
-        
-        - path: /tasks
-          method: GET
+        expect(res.status).toBe(406);
+        expect(res.body).toMatchInlineSnapshot(`
+            Object {
+              "actual": "Received 0 requests",
+              "expected": "At least 2 requests",
+              "message": "At least 2 requests is needed for verifying a sequence",
+              "records": Array [],
+            }
         `);
+    });
 
-    expect(res.status).toBe(406);
-    expect(res.body).toMatchInlineSnapshot(`
+    it('return the error from a failed verification', async () => {
+        const { apiServer } = await createServer({});
+
+        const res = await request(apiServer)
+            .put('/api/requests/sequence')
+            .set('content-type', 'application/json')
+            .send(
+                JSON.stringify([
+                    {
+                        path: '/tasks',
+                        method: 'POST',
+                    },
+                    {
+                        path: '/tasks',
+                        method: 'GET',
+                    },
+                ]),
+            );
+
+        expect(res.status).toBe(406);
+        expect(res.body).toMatchInlineSnapshot(`
             Object {
               "actual": Array [],
               "expected": Array [
@@ -89,71 +92,54 @@ test('return the error from a failed verification', async () => {
               "records": Array [],
             }
       `);
-});
+    });
 
-test('return accepted http 202', async () => {
-    // @ts-ignore: Jest Mock
-    fs.readFileSync.mockReturnValueOnce(`
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: GET
-            response:
-                body: Query worked
+    it('return accepted http 202', async () => {
+        const { apiServer, mockServer } = await createServer({});
 
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: POST
-            response:
-                body: Query worked
-    `);
+        await request(mockServer).post('/tasks');
+        await request(mockServer).get('/tasks');
 
-    const { apiServer, mockServer } = await createServer({});
+        const res = await request(apiServer)
+            .put('/api/requests/sequence')
+            .set('content-type', 'application/json')
+            .send(
+                JSON.stringify([
+                    {
+                        path: '/tasks',
+                        method: 'POST',
+                    },
+                    {
+                        path: '/tasks',
+                        method: 'GET',
+                    },
+                ]),
+            );
 
-    await request(mockServer).post('/tasks');
-    await request(mockServer).get('/tasks');
+        expect(res.status).toBe(202);
+    });
 
-    const res = await request(apiServer).put('/api/requests/sequence').set('content-type', 'application/x-yaml').send(`
-        - path: /tasks
-          method: POST
-        
-        - path: /tasks
-          method: GET
-    `);
+    it('return error for unmatched sequence', async () => {
+        const { mockServer, apiServer } = await createServer({});
+        await request(mockServer).get('/tasks');
+        await request(mockServer).get('/tasks');
 
-    expect(res.status).toBe(202);
-});
+        const res = await request(apiServer)
+            .put('/api/requests/sequence')
+            .set('content-type', 'application/json')
+            .send(
+                JSON.stringify([
+                    {
+                        path: '/tasks',
+                        method: 'POST',
+                    },
+                    {
+                        path: '/tasks',
+                        method: 'GET',
+                    },
+                ]),
+            );
 
-test('return error for unmatched sequence', async () => {
-    // @ts-ignore: Jest Mock
-    fs.readFileSync.mockReturnValueOnce(`
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: GET
-            response:
-                body: Query worked
-
-        -   name: test expectations
-            request:
-                path: /tasks
-                method: POST
-            response:
-                body: Query worked
-    `);
-
-    const { mockServer, apiServer } = await createServer({});
-    await request(mockServer).get('/tasks');
-    await request(mockServer).get('/tasks');
-
-    const res = await request(apiServer).put('/api/requests/sequence').set('content-type', 'application/x-yaml').send(`
-        - path: /tasks
-          method: POST
-        
-        - path: /tasks
-          method: GET
-        `);
-
-    expect(res.status).toBe(406);
+        expect(res.status).toBe(406);
+    });
 });
